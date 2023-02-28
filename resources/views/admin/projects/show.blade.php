5d72ac8bc5b33bfdd7ab70d8f52dcf5dff973631
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-28 15:58:47 +0100

    Add condition to see project's type, if present

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,6 +8,9 @@
             <div class="card-body">
               <h5 class="card-title">{{ $project->title }}</h5>
               <p>Created: <small>{{ $project->created }}</small></p>
+              @if ( {{ $project->type_id }} )
+              <p>Type: <small>{{ $project->type_id }}</small></p>
+              @endif
               <p class="card-text">{{ $project->description }}</p>
 
               {{-- Action buttons --}}
