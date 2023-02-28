2e92edfd29a6054c28be33373523cfa643d01853
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-28 19:34:59 +0100

    Update seeder to pick a random Type

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,12 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use App\Models\Project;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 class ProjectSeeder extends Seeder
 {
     /**
@@ -17,10 +19,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
         for($i = 0; $i < 50; $i++){
             $newProject = new Project();
-            $newProject->title = $faker->sentence(2);
-            $newProject->description = $faker->sentence(2);
+            $newProject->type_id = Type::inRandomOrder()->first()->id;
+            $newProject->title = $faker->sentence(4);
+            $newProject->description = $faker->paragraph();
             $newProject->link = $faker->unique()->url();
             $newProject->created = $faker->dateTimeThisYear();
             $newProject->slug = Str::slug($newProject->title);
